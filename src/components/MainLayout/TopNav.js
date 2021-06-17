df918fff642031b46db8062992de9d27f67cb55a
Lauralee Flores <flores.lauralee@gmail.com>
2021-06-16 17:57:32 -0700

    refactor navbar code

--- src/components/MainLayout/TopNav.js
+++ src/components/MainLayout/TopNav.js
@@ -1,16 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import {
-  width,
-  font,
-  gray,
-  blue,
-  spacing,
-  borderRadius,
-  screen,
-} from "../../utils";
+import { width, gray, blue, spacing, borderRadius, screen } from "../../utils";
 import { Link } from "gatsby";
 import Logo from "./Logo";
+import NavLinks from "./NavLinks";
 
 const TopNav = () => {
   return (
@@ -23,23 +16,7 @@ const TopNav = () => {
       <Hamburger>
         <Menu></Menu>
       </Hamburger>
-      <NavLinks>
-        <Link to="/" activeClassName="active">
-          Home
-        </Link>
-        <Link to="/about/" activeClassName="active">
-          About
-        </Link>
-        <Link to="/services/" activeClassName="active">
-          Services
-        </Link>
-        <Link to="/blog/" activeClassName="active">
-          Blog
-        </Link>
-        <Link to="/contact/" activeClassName="active" className="contact">
-          Contact
-        </Link>
-      </NavLinks>
+      <NavLinks />
     </Header>
   );
 };
@@ -53,7 +30,7 @@ const Header = styled.header`
   position: relative;
 `;
 const LogoWrapper = styled.div`
-  inline-size: 26rem;
+  inline-size: 20rem;
   padding-inline-start: ${spacing.s5};
   padding-block-start: ${spacing.s3};
   @media ${screen.xlg} {
@@ -111,50 +88,6 @@ const Hamburger = styled.div`
     &:after {
       background: ${blue.five};
       width: 30px;
-    }
-  }
-`;
-const NavLinks = styled.nav`
-  display: none;
-  @media ${screen.lg} {
-    display: block;
-  }
-  width: 61.25rem;
-  text-align: right;
-  flex-direction: row;
-  justify-content: flex-end;
-  a {
-    font-size: ${font.s20};
-    border-block-end: 2px solid ${gray.two};
-    line-height: 1.25;
-    text-decoration: none;
-    color: ${gray.four};
-    display: inline-block;
-    margin-inline-start: ${spacing.s5};
-    margin-inline-end: ${spacing.s5};
-    transition: 0.3s;
-    &:hover {
-      border-block-end: 2px solid ${blue.two};
-      color: ${gray.five};
-    }
-    &.active,
-    &.active:hover {
-      border-block-end: 2px solid ${blue.four};
-      color: ${gray.six};
-    }
-    &.contact {
-      border: 2px solid ${gray.five};
-      border-block-end: 2px solid ${gray.five};
-      padding: ${spacing.s3} ${spacing.s5};
-      border-radius: ${borderRadius.pill};
-      margin-inline-start: ${spacing.s4};
-      &:hover,
-      &.active,
-      &.active:hover {
-        background: ${blue.four};
-        border: 2px solid ${blue.four};
-        color: ${gray.white};
-      }
     }
   }
 `;
