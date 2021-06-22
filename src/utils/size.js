007a967e49f9593310e88c4c386af3f22c6859a3
Lauralee Flores <flores.lauralee@gmail.com>
2021-06-22 09:39:01 -0700

    add remaining responsive units for sizing

--- src/utils/size.js
+++ src/utils/size.js
@@ -2,34 +2,34 @@ export const base = {
   size: "1rem",
 };
 export const spacing = {
-  s1: "4px",
-  s2: "8px",
+  s1: "clamp(.15rem, .1rem + .2vw, .25rem)", // 4px
+  s2: "clamp(.25rem, .3rem + .3vw, .5rem)", // 8px
   s3: "clamp(.5rem, .5rem + .5vw, .75rem)", // 12px
-  s4: "clamp(.75rem, .5rem + .9vw, 1rem)", // 16px
-  s20: "clamp(.9rem, .8rem + .6vw, 1.2rem)", // 20px
-  s5: "clamp(1rem, 1rem + .9vw, 1.5rem)", // 24px
-  s6: "clamp(1.5rem, 1rem + 2vw, 2rem)", // 32px
-  s7: "48px", // 48px
-  s8: "clamp(2.827rem, 1rem + 6vw, 4rem)", // 64px
-  s9: "96px",
-  s10: "128px",
-  s11: "192px",
-  s12: "256px",
-  s13: "384px",
-  s14: "512px",
-  s15: "640px",
-  s16: "768px",
+  s4: "clamp(.75rem, .5rem + 1vw, 1rem)", // 16px
+  s20: "clamp(1rem, .8rem + 1vw, 1.25rem)", // 20px
+  s5: "clamp(1.3rem, 1rem + 1vw, 1.5rem)", // 24px
+  s6: "clamp(1.5rem, 1rem + 1.5vw, 2rem)", // 32px
+  s7: "clamp(2rem, 1rem + 3vw, 3rem)", // 48px
+  s8: "clamp(3rem, 1rem + 6vw, 4rem)", // 64px
+  s9: "clamp(4rem, 1rem + 8vw, 6rem)", // 96px
+  s10: "clamp(6rem, 1rem + 12vw, 8rem)", // 128px
+  s11: "clamp(8rem, 1rem + 13vw, 12rem)", // 192px
+  s12: "clamp(12rem, 1rem + 15vw, 16rem)", // 256px
+  s13: "clamp(13rem, 2rem + 21vw, 24rem)", // 384px
+  s14: "clamp(16rem, 2rem + 28vw, 32rem)", // 512px
+  s15: "clamp(22rem, 2rem + 34vw, 40rem)", // 640px
+  s16: "clamp(28rem, 2rem + 42vw, 48rem)", // 768px
 };
 export const font = {
   s1: "clamp(0.3rem, .3rem + .3vw, 0.422rem)", // 6.76px or 0.422rem
-  s2: "clamp(0.422rem, .5rem + .3vw, 0.563rem)", // 9px or 0.563rem
-  s3: "clamp(.5rem, .5rem + .5vw, .75rem)", // 12px
-  s4: "clamp(.75rem, .5rem + .9vw, 1rem)", // 16px
+  s2: "clamp(0.422rem, .5rem + .2vw, 0.563rem)", // 9px or 0.563rem
+  s3: "clamp(.5rem, .5rem + .4vw, .75rem)", // 12px
+  s4: "clamp(.75rem, .5rem + .8vw, 1rem)", // 16px
   s20: "clamp(.9rem, .8rem + .6vw, 1.25rem)", // 20px
-  s5: "clamp(1rem, 1rem + .9vw, 1.5rem)", // 24px or 1.5rem
-  s6: "clamp(1.5rem, 1rem + 2vw, 2rem)", // 32px or 2rem
-  s7: "clamp(2rem, 1rem + 4vw, 2.827rem)", // 45.23px or 2.827rem
-  s8: "clamp(2.827rem, 1rem + 6vw, 4rem)", // 64px or 4rem
+  s5: "clamp(1rem, 1rem + .8vw, 1.5rem)", // 24px or 1.5rem
+  s6: "clamp(1.5rem, 1rem + 1.5vw, 2rem)", // 32px or 2rem
+  s7: "clamp(2rem, 1rem + 3vw, 2.827rem)", // 45.23px or 2.827rem
+  s8: "clamp(2.827rem, 1rem + 5vw, 4rem)", // 64px or 4rem
   s9: "clamp(4rem, 1rem + 8vw, 5.653rem)", // 90.44px or 5.653rem
 };
 export const borderRadius = {
