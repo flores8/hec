331374fb64763ba9496d8218144d32738e44a58d
Lauralee Flores <flores.lauralee@gmail.com>
2021-06-26 13:04:06 -0700

    update the language a bit on our services

--- src/pages/services.js
+++ src/pages/services.js
@@ -23,27 +23,27 @@ const Services = () => {
           />
           <TwoColumnsImageLeft
             headline="Civil rights (Title IX, Title VI, ADA/504, etc.)"
-            description="HEC Partners believes that compliance is about more than just following the law. True compliance integrates a commitment to providing students, campus employees, and community partners with a safe environment in which to access and participate in education and educational activities."
+            description="We believe that compliance is about more than just following the law. True compliance integrates a commitment to providing students, campus employees, and community partners with a safe environment in which to access and participate in education and educational activities."
           />
           <TwoColumnsImageRight
             headline="Title IX"
-            description="We assist our clients in ensuring their education program or activities operate in a nondiscriminatory manner free of discrimination based on sex, including sexual orientation and gender identity. We provide policy development and review, investigate allegations of discrimiantion/harassment or retaliation for seeking redress under Title IX policies."
+            description="We help ensure your education program and activities operate in a nondiscriminatory manner free of discrimination based on sex, including sexual orientation and gender identity. We provide policy development and review, investigate allegations of discrimiantion/harassment or retaliation for seeking redress under Title IX policies."
           />
           <TwoColumnsImageLeft
             headline="Title VI"
-            description="Title VI of the Civil Rights Act of 1964 protects people from discrimination based on race, color or national origin in programs or activities that receive Federal financial assistance. HEC Partners works with clients to draft policies and procedures consistent with Title VI compliance, investigate complaints of discrimination, drafting of Title VI action plans. "
+            description="Title VI of the Civil Rights Act of 1964 protects people from discrimination based on race, color or national origin in programs or activities that receive Federal financial assistance. We will work with you to draft policies and procedures consistent with Title VI compliance, investigate complaints of discrimination, and draft Title VI action plans. "
           />
           <TwoColumnsImageRight
             headline="ADA/504"
-            description="Public colleges, universities, and private institutions of higher education that receive federal funding have a legal obligation to ensure that all environments across the campus are made accessible to all students, including students with disabilities. HEC Partners assists its clients in becoming familiar with legal mandates for digital and web-based technologies, responding to complaints of discrimination or failure to accommodate, and developing and reviewing institutional policies and training. "
+            description="Public colleges, universities, and private institutions of higher education that receive federal funding have a legal obligation to ensure that all environments across the campus are made accessible to all students, including students with disabilities. We can help you become familiar with legal mandates for digital and web-based technologies, responding to complaints of discrimination or failure to accommodate, and developing and reviewing institutional policies and training. "
           />
           <TwoColumnsImageLeft
             headline="Regulatory compliance"
-            description="Higher education is experiencing a time where regulatory activity is rapidly increasing. HEC Partners works with its clients to build an institutional Compliance Matrix. HEC Partners also assists its clients in creating and supporting an institutional culture of compliance, which includes effective policy development, training, internal communication strategies, and a quality assurance program. "
+            description="Higher education is experiencing a time where regulatory activity is rapidly increasing. HEC Partners works with its clients to build an institutional Compliance Matrix. We can also assists you in creating and supporting an institutional culture of compliance, which includes effective policy development, training, internal communication strategies, and a quality assurance program. "
           />
           <TwoColumnsImageRight
             headline="Human Resources"
-            description="The greatest strength of any organization is its people, and HEC Partners works with its clients to provide support and guidance their human resources staff needs to be in compliance with regulations and laws that impact institutions as a result of employee/employer interactions. HEC Partners delivers customized Human Resources support including crafting internal policies, investigating complaints of discrimination and harassment, and progressive discipline procedures."
+            description="The greatest strength of any organization is its people, and HEC Partners works with its clients to provide support and guidance their human resources staff needs to be in compliance with regulations and laws that impact institutions as a result of employee/employer interactions. We deliver customized Human Resources support including crafting internal policies, investigating complaints of discrimination and harassment, and progressive discipline procedures."
           />
           <TwoColumnsImageLeft
             headline="Policy Development"
@@ -51,7 +51,7 @@ const Services = () => {
           />
           <TwoColumnsImageRight
             headline="Federal Student Aid (Title IV)"
-            description="Get help with your Title IV Federal Student Aid program administration, compliance, operations, and automation. We experience with program reviews, single audits, internal audits, risk assessments, and general assessments of federal student aid administration and implementing proven methods to clear audit findings or potential risks of compliance issues. Just want to know the compliance health of your institution? We can do that too!"
+            description="Get help with your Title IV Federal Student Aid program administration, compliance, operations, and automation. We can help with program reviews, single audits, internal audits, risk assessments, and general assessments of federal student aid administration and implementing proven methods to clear audit findings or potential risks of compliance issues. Or we can simply give you an idea of the health of your institution."
           />
         </NonFullWidth>
       </Layout>
