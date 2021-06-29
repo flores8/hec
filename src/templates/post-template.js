901108710219eb4db747c8324c1562698d152ce9
Lauralee Flores <flores.lauralee@gmail.com>
2021-06-29 09:53:37 -0700

    style blog post

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -4,6 +4,8 @@ import Seo from "../components/seo";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import Layout from "../components/MainLayout/Layout";
+import styled from "styled-components";
+import { font, spacing, borderRadius, width } from "../utils";
 
 const postTemplate = ({ data }) => {
   const {
@@ -16,21 +18,52 @@ const postTemplate = ({ data }) => {
     <>
       <Seo title={title} subtitle={subtitle} author={author} />
       <Layout>
-        <h1 className="animate--immediate">{title}</h1>
-        <GatsbyImage
-          image={getImage(image.childrenImageSharp[0])}
-          alt={title}
-          className="animate--slow"
-        />
-        <article className="animate--slow">
-          <MDXRenderer>{body}</MDXRenderer>
-        </article>
+        <PostWrapper>
+          <Headline className="animate--immediate">{title}</Headline>
+          <Subtitle className="animate--slow">{subtitle}</Subtitle>
+          <GatsbyImage
+            image={getImage(image.childrenImageSharp[0])}
+            alt={title}
+            className="animate"
+          />
+          <Post className="animate flow">
+            <MDXRenderer>{body}</MDXRenderer>
+          </Post>
+        </PostWrapper>
       </Layout>
     </>
   );
 };
 
 export default postTemplate;
+
+const PostWrapper = styled.article`
+  margin-top: ${spacing.s8};
+  text-align: center;
+  .gatsby-image-wrapper {
+    border-radius: ${borderRadius.large};
+    max-width: ${width.xlg};
+    margin: auto;
+    margin-block-start: ${spacing.s8};
+    max-height: ${spacing.s16};
+  }
+`;
+const Headline = styled.h1`
+  font-size: ${font.s7};
+  line-height: 1.1;
+  max-width: ${width.md};
+  margin: auto;
+`;
+const Subtitle = styled.p`
+  font-size: ${font.s20};
+  padding-block-start: ${spacing.s4};
+  max-width: ${width.sm};
+  margin: auto;
+`;
+const Post = styled.article`
+  margin-top: ${spacing.s8};
+  text-align: left;
+`;
 
 export const query = graphql`
   query GetSinglePost($slug: String) {
